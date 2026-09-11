67cf135ea60e8fbcb5fce0690c37465268abf7ba
CRisdon45 <119950610+CRisdon45@users.noreply.github.com>
2026-09-11 16:47:12 -0700

    test: use compose interaction member assertions

--- app/src/test/java/com/example/OutlineControlStripTest.kt
+++ app/src/test/java/com/example/OutlineControlStripTest.kt
@@ -6,10 +6,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertContentDescriptionEquals
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.fetchSemanticsNode
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
